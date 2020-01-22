3c37ec2a7f6e02ef176482e717e9ef87e5a789aa
Josh Chasky <jchasky@gmail.com>
2020-01-22 09:27:16 -0500

    Clean up calls for packages we no longer build
    Scorpion Edits:
    Remove some of our old stuff too ;-)
    
    Change-Id: I9fc90f67f26a7e83786ec3fd839dbdc8dfcf1758

--- config/packages.mk
+++ config/packages.mk
@@ -2,28 +2,12 @@
 PRODUCT_PACKAGES += \
     DU-Themes \
     WallpaperPicker2
-#   CustomDoze \
-#   WeatherClient
 
 # Include librsjni explicitly to workaround GMS issue
 PRODUCT_PACKAGES += \
     librsjni
 
-# Fonts packages
-#PRODUCT_PACKAGES += \
-#    scorpion-fonts \
-#    Changelog.html
-
-# Omni packages
-#PRODUCT_PACKAGES += \
-#    OmniStyle
-
-# Subs OMS	
-#PRODUCT_PACKAGES += \
-#    SubstratumSignature
-
 # Allows registering device to Google easier for gapps
 # Integrates package for easier Google Pay fixing
 PRODUCT_PACKAGES += \
     sqlite3
-
