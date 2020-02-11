DirtyUnicorns/android_vendor_du

Switch to device package overlays instead of product
DEVICE_PACKAGE_OVERLAYS is used by several products that share a
common device model, which would be more fitting for a ROM vendor.

The PRODUCT_PACKAGE_OVERLAYS will override the DEVICE_PACKAGE_OVERLAYS
if they contain same resources, which is what we want.

Change-Id: I47466a391c105896fca47b0c3254d75b3ad9babe

config/common.mk
@@ -57,4 +57,4 @@ include vendor/du/config/branding.mk
 include vendor/themes/common.mk
 
 # Overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/du/overlay/common
+DEVICE_PACKAGE_OVERLAYS += vendor/du/overlay/common

wangdaning/vendor_crooked

config/common.mk
@@ -53,5 +53,5 @@ include vendor/scorpion/config/branding.mk
 include vendor/themes/common.mk
 
 # Overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/scorpion/overlay/common
+DEVICE_PACKAGE_OVERLAYS += vendor/scorpion/overlay/common
 