DirtyUnicorns/android_vendor_du

Emulator: Add pixel launcher / SystemUI permissions
This makes it boot again since the Emulator doesn't ship with gapps

Change-Id: Iddf243d571ebb0b45231accf478b02336343b5cd

config/common.mk
@@ -34,6 +34,10 @@ DU_PRODUCT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/etc/dirtyunicorns.rc:system/etc/init/dirtyunicorns.rc
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/etc/custom_permissions.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/custom_permissions.xml
+
 # Backup tool
 PRODUCT_COPY_FILES += \
     vendor/extras/build/tools/backuptool.sh:install/bin/backuptool.sh \

wangdaning/vendor_crooked

config/common.mk
@@ -34,6 +34,10 @@ SCORPION_PRODUCT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     vendor/scorpion/prebuilt/common/etc/dirtyunicorns.rc:system/etc/init/dirtyunicorns.rc
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/scorpion/prebuilt/common/etc/custom_permissions.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/custom_permissions.xml
+
 # Backup tool
 PRODUCT_COPY_FILES += \
     vendor/extras/build/tools/backuptool.sh:install/bin/backuptool.sh \
@@ -56,4 +60,3 @@ include vendor/themes/common.mk
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += vendor/scorpion/overlay/common
-