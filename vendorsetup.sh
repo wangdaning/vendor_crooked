15432b15b1e05d21bda88151404ebbdc17aa44dd
Danny Lin <danny@kdrag0n.dev>
2021-10-10 04:52:20 -0700

    vendorsetup: Update host metadata
    Matches the Android 12 build sandbox: https://android.googlesource.com/platform/build/+/refs/tags/android-12.0.0_r2/Changes.md

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,10 @@ function lunch_devices() {
     add_lunch_combo crooked_${device}-userdebug
 }
 
+# Override host metadata to make builds more reproducible and avoid leaking info
+export BUILD_USERNAME=nobody
+export BUILD_HOSTNAME=android-build
+
 for device in ${devices[@]}; do
     lunch_devices
 done
