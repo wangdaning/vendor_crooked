270f8465d716444ee9b87de2218d5765d8a825a0
neporood <neporood@gmail.com>
2022-01-07 15:06:28 -0600

    clean up the lunch menu [2/3]
    Change-Id: I58762b1a18f879ea25518b9ca46f2e04a9a9c15d

--- build/target/product/AndroidProducts.mk
+++ build/target/product/AndroidProducts.mk
@@ -14,30 +14,30 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_arm.mk \
-    $(LOCAL_DIR)/lineage_arm64.mk \
-    $(LOCAL_DIR)/lineage_x86.mk \
-    $(LOCAL_DIR)/lineage_x86_64.mk \
-    $(LOCAL_DIR)/lineage_tv_arm.mk \
-    $(LOCAL_DIR)/lineage_tv_arm64.mk \
-    $(LOCAL_DIR)/lineage_tv_x86.mk \
-    $(LOCAL_DIR)/lineage_tv_x86_64.mk \
-    $(LOCAL_DIR)/lineage_car_arm64.mk \
-    $(LOCAL_DIR)/lineage_car_x86_64.mk \
-    $(LOCAL_DIR)/lineage_sdk_phone_x86.mk \
-    $(LOCAL_DIR)/lineage_sdk_phone_x86_64.mk
+#PRODUCT_MAKEFILES := \
+#    $(LOCAL_DIR)/lineage_arm.mk \
+#    $(LOCAL_DIR)/lineage_arm64.mk \
+#    $(LOCAL_DIR)/lineage_x86.mk \
+#    $(LOCAL_DIR)/lineage_x86_64.mk \
+#    $(LOCAL_DIR)/lineage_tv_arm.mk \
+#    $(LOCAL_DIR)/lineage_tv_arm64.mk \
+#    $(LOCAL_DIR)/lineage_tv_x86.mk \
+#    $(LOCAL_DIR)/lineage_tv_x86_64.mk \
+#    $(LOCAL_DIR)/lineage_car_arm64.mk \
+#    $(LOCAL_DIR)/lineage_car_x86_64.mk \
+#    $(LOCAL_DIR)/lineage_sdk_phone_x86.mk \
+#    $(LOCAL_DIR)/lineage_sdk_phone_x86_64.mk
 
-COMMON_LUNCH_CHOICES := \
-    lineage_arm-userdebug \
-    lineage_arm64-userdebug \
-    lineage_x86-userdebug \
-    lineage_x86_64-userdebug \
-    lineage_tv_arm-userdebug \
-    lineage_tv_arm64-userdebug \
-    lineage_tv_x86-userdebug \
-    lineage_tv_x86_64-userdebug \
-    lineage_car_arm64-userdebug \
-    lineage_car_x86_64-userdebug \
-    lineage_sdk_phone_x86-userdebug \
-    lineage_sdk_phone_x86_64-userdebug
+#COMMON_LUNCH_CHOICES := \
+#    lineage_arm-userdebug \
+#    lineage_arm64-userdebug \
+#    lineage_x86-userdebug \
+#    lineage_x86_64-userdebug \
+#    lineage_tv_arm-userdebug \
+#    lineage_tv_arm64-userdebug \
+#    lineage_tv_x86-userdebug \
+#    lineage_tv_x86_64-userdebug \
+#    lineage_car_arm64-userdebug \
+#    lineage_car_x86_64-userdebug \
+#    lineage_sdk_phone_x86-userdebug \
+#    lineage_sdk_phone_x86_64-userdebug
