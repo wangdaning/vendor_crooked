f8b9220e06d9e4eafcf09bee9d73ea696198c4bb
Dabug123 <clarkbrunswick55@gmail.com>
2022-04-02 17:39:47 +0200

    Revert "StatixLauncher: google feed integration"
    This reverts commit 3c6fcdf8c78d5c4f9f01e74913593356f1804156.

--- app_override/StatixLauncher/src/com/statix/launcher/StatixLauncher.java
+++ app_override/StatixLauncher/src/com/statix/launcher/StatixLauncher.java
@@ -17,13 +17,7 @@
 package com.statix.launcher;
 
 import com.android.launcher3.uioverrides.QuickstepLauncher;
-import com.android.systemui.plugins.shared.LauncherOverlayManager;
 
 public class StatixLauncher extends QuickstepLauncher {
-
-    @Override
-    protected LauncherOverlayManager getDefaultOverlay() {
-        return new OverlayCallbackImpl(this);
-    }
 
 }
