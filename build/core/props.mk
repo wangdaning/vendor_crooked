f2b2c8c6a780971142ebc0e91829fac6e0ed29f6
koen01 <koen.erens@gmail.com>
2022-04-28 08:26:45 +0000

    change props for crooked

--- build/core/props.mk
+++ build/core/props.mk
@@ -3,9 +3,9 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.stock_fingerprint=$(BUILD_FINGERPRINT)
 
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.statix.version=$(STATIX_BASE_VERSION)-$(STATIX_BUILD_TYPE)-$(BUILD_DATE)-$(BUILD_TIME) \
-    ro.statix.base.version=$(STATIX_BASE_VERSION) \
-    ro.mod.version=$(BUILD_ID)-$(BUILD_DATE)-$(STATIX_BASE_VERSION) \
-    ro.statix.fingerprint=$(ROM_FINGERPRINT) \
-    ro.statix.buildtype=$(STATIX_BUILD_TYPE) \
-    ro.statix.device=$(TARGET_DEVICE)
+    ro.crooked.version=$(CROOKED_BASE_VERSION)-$(CROOKED_BUILD_TYPE)-$(BUILD_DATE)-$(BUILD_TIME) \
+    ro.crooked.base.version=$(CROOKED_BASE_VERSION) \
+    ro.mod.version=$(BUILD_ID)-$(BUILD_DATE)-$(CROOKED_BASE_VERSION) \
+    ro.crooked.fingerprint=$(ROM_FINGERPRINT) \
+    ro.crooked.buildtype=$(CROOKED_BUILD_TYPE) \
+    ro.crooked.device=$(TARGET_DEVICE)
