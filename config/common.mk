4d7fdd0d50a3d56ada4e91ebee9599a5b4f9d5e9
koen01 <koen.erens@gmail.com>
2022-05-17 10:57:05 +0000

    faceunlock set

--- config/common.mk
+++ config/common.mk
@@ -86,14 +86,15 @@ PRODUCT_PACKAGES += \
     AndroidBlackThemeOverlay
 
 # Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
 PRODUCT_PACKAGES += \
     FaceUnlockService
-    
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.face_unlock_service.enabled=true
-    
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
 
 # Artifact path requirements
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
