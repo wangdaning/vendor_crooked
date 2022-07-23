ccb4f133c5af331a21a704e412958d0cb444cf4a
Dabug123 <clarkbrunswick55@gmail.com>
2022-07-23 13:57:23 +0000

    fonts:coolstory

--- config/overlay.mk
+++ config/overlay.mk
@@ -18,6 +18,7 @@
 PRODUCT_PACKAGES += \
     FontArvoLatoOverlay \
     FontBarlowOverlay \
+    FontCoolstoryOverlay \
     FontInterOverlay \
     FontNothiingDotOverlay \
     FontNothingDotHeadlineOverlay \
