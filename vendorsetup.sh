212331a37e3813774c2237047ed2673e6de57add
neporood <neporood@gmail.com>
2022-10-01 18:22:46 -0500

    vendorsetup: remove lunch device function
    - We are using COMMON_LUNCH_CHOICES

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,14 +1,4 @@
-devices=( 'coral' 'crosshatch' 'oriole' 'raven' 'sunfish')
-
-function lunch_devices() {
-    add_lunch_combo crooked_${device}-user
-    add_lunch_combo crooked_${device}-userdebug
-}
-
 # Override host metadata to make builds more reproducible and avoid leaking info
 export BUILD_USERNAME=nobody
 export BUILD_HOSTNAME=android-build
 
-for device in ${devices[@]}; do
-    lunch_devices
-done
